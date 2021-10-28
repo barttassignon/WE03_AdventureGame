16d11dcfc48044185dc8e9cea8111fee6182abbb
Bart Tassignon <bart.tassignon@student.ehb.be>
2021-10-28 19:52:37 +0200

    Game logic start

--- WE03_AdventureGame/Program.cs
+++ WE03_AdventureGame/Program.cs
@@ -1,21 +1,54 @@
 ﻿using AdventureLib;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using WE03_ClassLibrary_AG.Classes;
 using WE03_ClassLibrary_AG.Classes.Items;
 
+
 namespace WE03_ClassLibrary_AG
 {
-    class Program
+    static class Program
     {
+        private static Game game;
 
+        private const ConsoleColor highlightColor = ConsoleColor.White;
+        private const ConsoleColor badColor = ConsoleColor.Red;
+        private const ConsoleColor goodColor = ConsoleColor.Green;
+        private const ConsoleColor neutralColor = ConsoleColor.Blue;
 
-        static void Main(string[] args)
+        static void Main()
         {
+            TextInfo testinfo = new CultureInfo("nl-BE", true).TextInfo;
+            game = InitGame(ReadStringFromConsole("Before you embark on your grand adventure; what is your name? "));
+            int headerHeight;
+            int prevLeft, prevTop;
 
-            bool doorgaan = true;
-            while (doorgaan)
+            Console.Clear();
+
+
+
+            while (!game.GameOver)
             {
+                prevLeft = Console.CursorLeft;
+                prevTop = Console.CursorTop;
+
+                // kamerinfo weergeven bovenaan scherm
+                string lijntje = new string('=', game.World.CurrentRoom.Description.Length);
+                headerHeight = 5 + game.World.CurrentRoom.ToString().Length / Console.WindowWidth;
+                ClearLine(0, headerHeight);
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine(lijntje);
+                Console.WriteLine(game.World.CurrentRoom);
+                Console.WriteLine(lijntje);
+
+                // als er items liggen in kamer deze weergeven
+                if (game.World.CurrentRoom.Items?.Count > 0)
+                {
+
+                }
+
+
                 Console.WriteLine("What do you want to do?");
                 string invoer = Console.ReadLine();
                 List<string> woorden;
@@ -36,12 +69,18 @@ namespace WE03_ClassLibrary_AG
                     case Parser.CommandType.Move:
                         break;
                     case Parser.CommandType.Exit:
-                        doorgaan = false;
+                        // gebruiker wil spel verlaten
+                        game.GameOver = true;
                         break;
                     default:
                         break;
                 }
+                Console.ResetColor();
             }
+
+            Console.ForegroundColor = (game.GameWon) ? goodColor : badColor;
+            Console.WriteLine(game.GameOverMessage);
+            Console.ResetColor();
 
         }
 
@@ -52,6 +91,7 @@ namespace WE03_ClassLibrary_AG
             Player player = new Player(playerName, new List<Interfaces.IItem> { beer });
 
             Room towncentre = new Room { Name = "Towncentre", Description = "The centre of the town. To the left is the local bar and up ahead is the forest." };
+
             Room forest = new Room { Name = "Forest", Description = "A darke dense forest. A clearing in the middle reveals a strange wooden door" };
             forest.Items = new List<Interfaces.IItem> { new Door() };
 
@@ -67,6 +107,46 @@ namespace WE03_ClassLibrary_AG
 
             World world = new World(towncentre);
             return new Game(player, world);
+        }
+
+        /// <summary>
+        /// asks for input as long as user's input is null, empty, or consists of white-space characters.
+        /// </summary>
+        /// <param name="message">The message to display in the console</param>
+        /// <returns>string containing the user's input</returns>
+        private static string ReadStringFromConsole(string message)
+        {
+            string input;
+            bool inputIsInvalid;
+
+            Console.Write(message);
+            Console.ForegroundColor = highlightColor;
+            input = Console.ReadLine();
+            inputIsInvalid = string.IsNullOrEmpty(input);
+            if (inputIsInvalid)
+            {
+                Console.ForegroundColor = badColor;
+                Console.WriteLine("That is not a valid input.\n");
+            }
+            Console.ResetColor();
+            while (inputIsInvalid) ;
+
+            return input;
+        }
+
+        private static void ClearLine(int top, int amount)
+        {
+            int origLeft = Console.CursorLeft;
+            int origTop = Console.CursorTop;
+
+            Console.SetCursorPosition(0, top);
+
+            for (int i = 0; i < amount; i++)
+            {
+                Console.Write(new string(' ', Console.BufferWidth));
+            }
+
+            Console.SetCursorPosition(origLeft, origTop);
         }
     }
 }
